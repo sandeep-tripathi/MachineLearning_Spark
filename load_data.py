919a139005bc95d0b3ea88e3a277c4d3c5a58480
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-09 21:52:15 +0100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -79,3 +79,13 @@ flights_assembled = assembler.transform(flights)
 
 # Check the resulting column
 flights_assembled.select('features', 'delay').show(5, truncate=False)
+
+
+
+
+# Split into training and testing sets in a 80:20 ratio
+flights_train, flights_test = flights.randomSplit([0.8, 0.2], seed=43)
+
+# Check that training set has around 80% of records
+training_ratio = flights_train.count() / flights.count()
+print(training_ratio)

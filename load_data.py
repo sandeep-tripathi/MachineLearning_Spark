439e38480280824a4a998c70fe449313ff9ae85a
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-09 20:56:29 +0100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,3 +1,4 @@
+#Aviation CSV file
 # Read data from CSV file
 flights = spark.read.csv("flights.csv",
                          sep=",",
@@ -26,3 +27,18 @@ flights_valid_delay = flights_drop_column.filter('delay IS NOT NULL')
 # Remove records with missing values in any column and get the number of remaining rows
 flights_none_missing = flights_valid_delay.dropna()
 print(flights_none_missing.count())
+
+
+## Column Manipulation
+# Import the required function
+from pyspark.sql.functions import round
+
+# Convert 'mile' to 'km' and drop 'mile' column (1 mile is equivalent to 1.60934 km)
+flights_km = flights.withColumn('km', round(flights.mile * 1.60934, 0)) \
+                    .drop('mile')
+
+# Create 'label' column indicating whether flight delayed (1) or not (0)
+flights_km = flights_km.withColumn('label', (flights_km.delay >= 15).cast('integer'))
+
+# Check first five records
+flights_km.show(5)

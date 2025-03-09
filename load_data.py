6c62ab1617d47115cd5295ba6fc60831af63d300
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-09 21:54:46 +0100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -105,3 +105,17 @@ tree_model = tree.fit(flights_train)
 # Create predictions for the testing data and take a look at the predictions
 prediction = tree_model.transform(flights_test)
 prediction.select('label', 'prediction', 'probability').show(5, False)
+
+## Confusion Matrix and accuracy
+# Create a confusion matrix
+prediction.groupBy('label', 'prediction').count().show()
+
+# Calculate the elements of the confusion matrix
+TN = prediction.filter('prediction = 0 AND label = prediction').count()
+TP = prediction.filter('prediction = 1 AND label = prediction').count()
+FN = prediction.filter('prediction = 0 AND label != prediction').count()
+FP = prediction.filter('prediction = 1 AND label != prediction').count()
+
+# Accuracy measures the proportion of correct predictions
+accuracy = (TN + TP) / (TN + TP + FN + FP)
+print(accuracy)

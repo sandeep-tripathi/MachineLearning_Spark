42e5c080d227bd7e1ca3e323e3fb31b07de4af62
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-10 17:04:49 +0100

    Create forrest.py

---
+++ forrest.py
@@ -0,0 +1,14 @@
+# Create a random forest classifier
+forest = RandomForestClassifier()
+
+# Create a parameter grid
+params = ParamGridBuilder() \
+            .addGrid(forest.featureSubsetStrategy, ['all', 'onethird', 'sqrt', 'log2']) \
+            .addGrid(forest.maxDepth, [2, 5, 10]) \
+            .build()
+
+# Create a binary classification evaluator
+evaluator = BinaryClassificationEvaluator()
+
+# Create a cross-validator
+cv = CrossValidator(estimator=forest, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)

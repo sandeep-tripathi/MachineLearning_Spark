bdf5b283ec8e2f3bece2fd7d7dbd268d85242d63
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-10 16:40:13 +0100

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,1 +1,12 @@
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
 
+# Create a regression object and train on training data
+regression = LinearRegression(labelCol='duration').fit(flights_train)
+
+# Create predictions for the testing data and take a look at the predictions
+predictions = regression.transform(flights_test)
+predictions.select('duration', 'prediction').show(5, False)
+
+# Calculate the RMSE
+RegressionEvaluator(labelCol='duration').evaluate(predictions)

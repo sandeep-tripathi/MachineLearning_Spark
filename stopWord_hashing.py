a23eb0af5cdcc6f7fa49bdad2bd148e2ce09ebf2
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-10 13:06:25 +0100

    Create stopWord_hashing.py

---
+++ stopWord_hashing.py
@@ -0,0 +1,15 @@
+from pyspark.ml.feature import StopWordsRemover, HashingTF, IDF
+
+# Remove stop words.
+wrangled = StopWordsRemover(inputCol='words', outputCol='terms')\
+      .transform(sms)
+
+# Apply the hashing trick
+wrangled = HashingTF(inputCol='terms', outputCol='hash', numFeatures=1024)\
+      .transform(wrangled)
+
+# Convert hashed symbols to TF-IDF
+tf_idf = IDF(inputCol='hash', outputCol='features')\
+      .fit(wrangled).transform(wrangled)
+      
+tf_idf.select('terms', 'features').show(4, truncate=False)

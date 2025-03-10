8cedb605bff8c03178f1bad3ddc5ba2a712ead19
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-10 16:57:34 +0100

    Create crossvalidate.py

---
+++ crossvalidate.py
@@ -0,0 +1,14 @@
+# Create an empty parameter grid
+params = ParamGridBuilder().build()
+
+# Create objects for building and evaluating a regression model
+regression = LinearRegression(labelCol='duration')
+evaluator = RegressionEvaluator(labelCol='duration')
+
+# Create a cross validator
+cv = CrossValidator(estimator=regression, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+# Train and test model on multiple folds of the training data
+cv = cv.fit(flights_train)
+
+

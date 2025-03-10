1e49399a701c755a6c3af633ccc4421cd9bc5303
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-10 17:05:17 +0100

    Update forrest.py

--- forrest.py
+++ forrest.py
@@ -12,3 +12,18 @@ evaluator = BinaryClassificationEvaluator()
 
 # Create a cross-validator
 cv = CrossValidator(estimator=forest, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+
+# Average AUC for each parameter combination in grid
+print(cv.avgMetrics)
+
+# Average AUC for the best model
+print(max(cv.avgMetrics))
+
+# What's the optimal parameter value for maxDepth?
+print(cv.bestModel.explainParam('maxDepth'))
+# What's the optimal parameter value for featureSubsetStrategy?
+print(cv.bestModel.explainParam('featureSubsetStrategy'))
+
+# AUC for best model on testing data
+print(evaluator.evaluate(cv.transform(flights_test)))
